cb5dd832cb9a9a59f43c49d5f3e7dbe02ca372ff
AntonBereza1995 <80589071+AntonBereza1995@users.noreply.github.com>
2021-09-09 20:22:45 +0300

    ready

---
+++ src/js/menu.js
@@ -0,0 +1,6 @@
+  
+import menuList from '../db/menu.json'
+import menuTpl from '../templates/menu.hbs'
+import refs from './refs'
+
+refs.menuContainer.insertAdjacentHTML('beforeend', menuTpl(menuList))
